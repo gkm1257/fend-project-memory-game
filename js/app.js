8196fb7889ac1b922dff41974204928a51bae4b8
gkm1257 <gkm1257@gmail.com>
2017-10-21 13:12:44 +0800

    fix: Exclude class "open" when clicked

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 cardList.click(function() {
-    if ($(this).hasClass("match") === false) {
+    if ($(this).hasClass("match") === false && $(this).hasClass("open") === false) {
         showCard($(this));
         addToOpen($(this));
         if (cardOpenList.length > 1) {
